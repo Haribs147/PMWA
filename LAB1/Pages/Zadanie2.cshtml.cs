29e1e5174d479cf9e8a264b1c342c3035496aa87
Michal <m.jagodzinski1234@gmail.com>
2024-10-05 17:49:32 +0200

    zadanie 2 bug fix

--- LAB1/Pages/Zadanie2.cshtml.cs
+++ LAB1/Pages/Zadanie2.cshtml.cs
@@ -5,6 +5,13 @@ namespace LAB1.Pages
 {
     public class Zadanie2Model : PageModel
     {
+         public class UserModel
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Message { get; set; }
+        }
+    
         public UserModel UserData { get; set; } = new UserModel();
         public bool ShowData { get; set; } = false;
 
@@ -17,13 +24,32 @@ namespace LAB1.Pages
 
             // Ustawiamy flagę, żeby dane były widoczne w widoku po przesłaniu
             ShowData = true;
+
+            //VIEWDATA
+            // ViewData["Name"] = name;
+            // ViewData["Email"] = email;
+            // ViewData["Message"] = message;
         }
+
+        //Sessions
+
+        // public void OnPostSession(string name, string email, string message)
+        // {
+        //     // Ustawiamy wartości w sesji
+        //     HttpContext.Session.SetString("Name", name);
+        //     HttpContext.Session.SetString("Email", email);
+        //     HttpContext.Session.SetString("Message", message);
+        // }
+
+        // // Metoda do pobierania danych z sesji
+        // public void LoadDataFromSession()
+        // {
+        //     ViewData["Name"] = HttpContext.Session.GetString("Name");
+        //     ViewData["Email"] = HttpContext.Session.GetString("Email");
+        //     ViewData["Message"] = HttpContext.Session.GetString("Message");
+        // }
+
     }
 
-    public class UserModel
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Message { get; set; }
-    }
+   
 }
